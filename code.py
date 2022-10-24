b64aeada1f5feaadd17f3a9f73d9b974cd3bb6aa
Robert Jordan <robert@twingate.com>
2022-10-24 10:13:06 -0700

    updates

--- code.py
+++ code.py
@@ -9,11 +9,21 @@ led.direction = digitalio.Direction.OUTPUT
 relay = digitalio.DigitalInOut(board.GP16)
 relay.direction = digitalio.Direction.OUTPUT
 
- 
+button = digitalio.DigitalInOut(board.GP15)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
+unlock_duration = 5
+elapsed_time = 0
+
 while True:
+    while elapsed_time < unlock_duration:
+        if button.value:
+            elapsed_time += 0.001
+            time.sleep(0.001)
+            print(elapsed_time)
+        else:
+            elapsed_time = 0
+
     led.value = True
     relay.value = True
-    time.sleep(0.5)
-    led.value = False
-    relay.value = False
-    time.sleep(0.5)
